9970ba5ccb15a5ef562d9a3f0e12575fc8572e1d
Raul-Docs <raul.cal.bar@gmail.com>
2022-02-07 17:38:32 +0100

    Primer commit, añadida clase Main en branch main.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        //Cada uno crea su rama y mergeamos a la main.
+        //Cuando esten todas las ramas unidas en la rama main, modificamos esta Clase Main
+        //Los conflictos al hacer push de los cambios en la Clase Main los resolvemos por Whatsapp
+    }
+}
