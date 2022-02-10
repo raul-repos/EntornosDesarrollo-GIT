7bde5db0ee3d626c87ba3a7f824ac9e46ef9dbd7
Raul-Docs <raul.cal.bar@gmail.com>
2022-02-10 17:30:51 +0100

    Division.java, 4/4 metodos hechos

--- src/com/github/raul_repos/Division.java
+++ src/com/github/raul_repos/Division.java
@@ -1,31 +1,25 @@
 package com.github.raul_repos;
-import java.lang.Math.*;
 /**
-     * Clase empleada para dividir numeros entre si.
-     *
-     * Estos numeros deben ser positivos.
-     */
+ * Clase empleada para dividir numeros entre si.
+ *
+ * Estos numeros deben ser positivos.
+ */
 public class Division {
         /**
          * Metodo que divide un numero real por otro.
          *
          * El parametro <i>a</i> actua como dividendo, siendo el parametro <i>b</i> el que actua como divisor. Ninguno de los dos puede ser 0.
-         * <ul>
-         *     <li>En caso en el que el resto sea distinto a 0, se continua hasta un máximo de dos números decimales, truncando en la centena.</li>
-         *     <li>En caso de que la division de un numero periodico, el resultado se redondea a la alza en la centena.</li>
-         * </ul>
          *
          * @param a numero real dividendo (numero que se divide).
          * @param b numero real divisor (numero por el que se divide).
          * @return numero real resultante de la division del parametro <i>a</i> entre el parametro. <i>b</i>
          * @throws IllegalArgumentException uno de los parametros introducidos es cero (0).
          */
-        // TODO
-        public double Division(double a, double b) {
+        public double dividir(double a, double b) {
                 if (a == 0 || b == 0){
-                        throw new IllegalArgumentException("Los numeros deben ser distintos de cero");
+                        throw new IllegalArgumentException("Uno de los parametros introducido es cero (0)");
                 }
-           return(Math.floor(a/b);
+           return a/b;
         }
 
         /**
@@ -42,14 +36,15 @@ public class Division {
          * @return numero real resultante de la division del parametro <i>a</i> entre el parametro <i>b</i>.
          * @throws IllegalArgumentException uno de los parametros introducidos es cero (0).
          */
-        // TODO
-        public int Division(int a, int b){
-
+        public double dividir(int a, int b){
+                if (a == 0 || b == 0){
+                        throw new IllegalArgumentException("Uno de los parametros introducido es cero (0)");
+                }
+                return Math.round(a/b);
         }
 
         /**
          * Metodo que devuelve el inverso de un numero real.
-         *
          * @param a numero real.
          * @return devuelve el inverso del numero real.
          * @throws IllegalArgumentException el parametro introducido no puede ser cero (0).
