be8afdfa5156afeff674972d989dd399f88b45f3
Raul-Docs <raul.cal.bar@gmail.com>
2022-02-14 20:29:15 +0100

    Hecho cambio en Main desde Suma

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import com.github.raul_repos.Suma;
+
 public class Main {
     public static void main(String[] args){
         //Cada uno crea su rama y mergeamos a la main.
         //Cuando esten todas las ramas unidas en la rama main, modificamos esta Clase Main
         //Los conflictos al hacer push de los cambios en la Clase Main los resolvemos por Whatsapp
+        Suma s = new Suma();
+        int SegundoMetodo = s.sumar(1,2);
+        System.out.println(SegundoMetodo);
     }
 }
